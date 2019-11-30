09fc6d5ec9e7a3185c468b3432af2e5a4b368204
Byrne Hollander <bhollander823@gmail.com>
2019-11-30 17:07:55 -0500

    More setup

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import penrose from './penrose.svg';
+
+/* 
+This file should really end in .tsx and use typescript but 
+I still need to figure out how to use it!
+*/
+
+const CenteredText = styled.div`
+    text-align: center;
+`;
+
+const Header = styled.div`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+const AppLogo = styled.img`
+    height: 40vmin;
+`;
+
+const AppLink = styled.a`
+    color: #09d3ac;
+`;
+
+function App() {
+    return (
+        <CenteredText>
+            <Header>
+                <AppLogo src={penrose} alt="penrose triangle" />
+                <p>This is the starting place for the Strange Loops project!</p>
+                <AppLink
+                    href="https://github.com/strange-loops/strange-loops"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    See the README
+                </AppLink>
+            </Header>
+        </CenteredText>
+    );
+}
+
+export default App;
